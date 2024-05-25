277ecf2a87c097135c1dc0fd84cdc7e263239018
Mingze Lin <mingzelin@Mingzes-MacBook-Pro.local>
2024-05-25 11:15:15 +0800

    first commit

--- contactme.php
+++ contactme.php
@@ -1,4 +1,4 @@
-<?php   
+<?php
     require("./mailing/mailfunction.php");
 
     $name = $_POST["name"];
@@ -8,9 +8,9 @@
 
     $body = "<ul><li>Name: ".$name."</li><li>Phone: ".$phone."</li><li>Email: ".$email."</li><li>Message: ".$message."</li></ul>";
 
-    $status = mailfunction("", "Company", $body); //reciever
+    $status = mailfunction("ml4722@columbia.edu", "Company", $body); //reciever
     if($status)
         echo '<center><h1>Thanks! We will contact you soon.</h1></center>';
     else
-        echo '<center><h1>Error sending message! Please try again.</h1></center>';    
+        echo '<center><h1>Error sending message! Please try again.</h1></center>';
 ?>
